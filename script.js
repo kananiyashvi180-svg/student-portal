9cd30da0a764c44fb004be8f85a58e8a4454ec76
kananiyashvi180-svg <kananiyashvi180@gmail.com>
2025-08-22 23:26:45 +0530

    Add course registration feature

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ console.log('Student Portal initialized');
 function login() {
   console.log('Login functionality coming soon...');
 }
+function registerCourse() {
+  console.log('Course registration functionality added');
+}
